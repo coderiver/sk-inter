2da1cfbd7daaa1531f274ce9df4f2e6daeabb7ca
moroz <morozarturv@gmail.com>
2014-06-16 15:37:38 +0300

    upd contacts map

--- js/common.js
+++ js/common.js
@@ -7,5 +7,19 @@ head.ready(function() {
 		$(".js-popup").hide();
 	});
 
-	console.log($('body').html());
+	//
+	function map_height() {
+		var map = $('.map__el'),
+				map_top = map.offset().top,
+				footer = $('.footer'),
+				footer_height = footer.outerHeight(),
+				window_height = $(window).height();
+		map.height(window_height - (map_top + footer_height));
+	};
+	map_height();
+
+	$(window).resize(function(){
+		map_height();
+	});
+	
 });
